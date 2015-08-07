896992d5691dcb7085a5fb1a19dcffba8ce440a3
Brian Woodward <brian.woodward@gmail.com>
2015-08-07 16:01:34 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * ask-once <https://github.com/doowb/ask-once>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
