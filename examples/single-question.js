58a195fef83e3ab07cefb746234f07d9de4122d2
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-30 02:30:34 -0400

    update example with latest `data-store` config
    also just pass `arvg` directly as options

--- examples/single-question.js
+++ examples/single-question.js
@@ -1,15 +1,20 @@
 'use strict';
 
-var argv = require('minimist')(process.argv.slice(2));
-
 var path = require('path');
-var inquirer = require('inquirer');
-var Questions = require('question-cache');
-var DataStore = require('data-store');
+var questions = require('question-cache')();
 
-var store = new DataStore('ask-once-example', {cwd: path.join(__dirname, '.data')});
-var questions = new Questions({
-  inquirer: inquirer
+/*
+ * Provide options at the commandline
+ *  - [i]nit  => clear the answer from the store
+ *  - [f]orce => force asking the question
+ */
+
+var argv = require('minimist')(process.argv.slice(2), {
+  alias: {force: 'f', init: 'i'}
+});
+
+var store = require('data-store')('ask-once-example', {
+  cwd: path.join(__dirname, '.data')
 });
 
 questions
@@ -19,21 +24,7 @@ questions
 
 var ask = require('..')(questions, store);
 
-/*
- * Provide options at the commandline
- *  - [i]nit => clear the answer from the store
- *  - [f]orce => force asking the question
- */
-
-var options = {};
-if (argv.i || argv.init) {
-  options.init = true;
-}
-if (argv.f || argv.force) {
-  options.force = true;
-}
-
-ask('username', options, function (err, answer) {
+ask('username', argv, function (err, answer) {
   if (err) return console.error(err);
   console.log('You\'re username is', answer);
 });
